66754842540fe8eb741b6d1745af99215fb7d17c
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-05-17 22:52:23 -0500

    Update a bit the handling of single audio/text streams

--- canonicalize.py
+++ canonicalize.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # An input argument that takes a list of filepaths
 
-    _ = parser.add_argument("--input", "-i", nargs="+", help="Input file(s)", required=True)
+    _ = parser.add_argument("input", nargs="+", help="Input file(s), format is <filename>@@<Title>@@<Language> where the two extra fields are only relevant for extra audio/subtitle tracks.")
     args = parser.parse_args()
 
     inputs: list[str] = cast(list[str], args.input)
@@ -18,8 +18,23 @@ if __name__ == "__main__":
     containers: list[MediaContainer] = []
 
     for i in inputs:
-        file_cont = MediaContainer(i)
+        input_file = i
+        if '@@' in i:
+            input_file = i.split('@@')[0]
+
+        file_cont = MediaContainer(input_file)
         file_cont.analyze()
+        stream_lengths = (len(file_cont.video), len(file_cont.audio), len(file_cont.subtitle))
+        if len(file_cont.subtitle) == 1 and sum(stream_lengths) == 1:
+            title = i.split('@@')[1]
+            language = i.split('@@')[2]
+            file_cont.subtitle[0].title = title
+            file_cont.subtitle[0].language = language
+        if len(file_cont.audio) == 1 and sum(stream_lengths) == 1:
+            title = i.split('@@')[1]
+            language = i.split('@@')[2]
+            file_cont.audio[0].title = title
+            file_cont.audio[0].language = language
         containers.append(file_cont)
 
         print(f"File {i}")
