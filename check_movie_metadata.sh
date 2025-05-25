c441b126b1daf80cd0129382a261c398de80b549
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-05-24 23:37:47 -0500

    Improve check movie metadata

--- check_movie_metadata.sh
+++ check_movie_metadata.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # metadata.sh <title> <year> [movie|series]
 TITLE="$1"
-#YEAR="$2"
+YEAR="$2"
 TYPE="${3:-movie}"   # default to movie
 
 if [[ -z "$OMDB_API_KEY" ]]; then
@@ -11,10 +11,10 @@ fi
 
 case "$TYPE" in
   movie)
-    URL="http://www.omdbapi.com/?t=$(printf %s "$TITLE" | jq -sRr @uri)&apikey=$OMDB_API_KEY"
+    URL="http://www.omdbapi.com/?t=$(printf %s "$TITLE" | jq -sRr @uri)&y=$YEAR&apikey=$OMDB_API_KEY"
     ;;
   series)
-    URL="http://www.omdbapi.com/?t=$(printf %s "$TITLE" | jq -sRr @uri)&type=series&apikey=$OMDB_API_KEY"
+    URL="http://www.omdbapi.com/?t=$(printf %s "$TITLE" | jq -sRr @uri)&y=$YEAR&type=series&apikey=$OMDB_API_KEY"
     ;;
   *)
     echo "Unknown type: $TYPE" >&2
